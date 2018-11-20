ac27766649a5210e3bbf95b4f777d7b7b964316a
yesyxv <belhassnaoui.youness@gmail.com>
2018-11-20 15:41:11 +0100

    cypress CI with travis

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -1,5 +1,7 @@
 describe('The Home Page', function() {
     it('successfully loads', function() {
       cy.visit('/')
+      cy.get('#formHorizontalEmail').type('leleux@vinci.be')
+      cy.get('#formHorizontalPassword').type('leleux')
     })
 })
